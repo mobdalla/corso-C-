429a2bfd1dc3a6167a4b840626ad21199ddf538d
mobdalla <76433493+mobdalla@users.noreply.github.com>
2024-06-12 12:40:54 +0200

    aggiuge il file c#

---
+++ ListaGenerica/ListaGenerica/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ListaGenerica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
